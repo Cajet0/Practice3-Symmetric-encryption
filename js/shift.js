026bfe60785b58027952930696563452a45e4b62
Oliver Mendoza <oliver.r.2@hotmail.com>
2017-05-28 21:50:26 -0500

    Correction buildMatrix function

--- js/shift.js
+++ js/shift.js
@@ -1,21 +1,21 @@
 var str, opc;
-var M = [[]]; // Message block
+var M = []; // Message block
 
-function magia(pal, opcion){
+/*function magia(pal, opcion){
     str = pal;
     if(opcion.localeCompare("Encriptar") == 0) caesarShift(pal,2);
     if(opcion.localeCompare("Desencriptar") == 0) caesarShift(pal,-2);
-}
+}*/
 
 function caesarShift(str, amount) {
 
     if((str.length < 5) || (str.length > 20 )){
-    		alert('Palabra entre cinco y veinte caracteres');
+            alert('Palabra entre cinco y veinte caracteres');
     }
     else {
-    	// Wrap the amount
+        // Wrap the amount
     if (amount < 0)
-    	return caesarShift(str, amount + 26);
+        return caesarShift(str, amount + 26);
 
     // Make an output variable
     var output = '';
@@ -23,31 +23,31 @@ function caesarShift(str, amount) {
     // Go through each character
     for (var i = 0; i < str.length; i ++) {
 
-    	// Get the character we'll be appending
-    	var c = str[i];
+        // Get the character we'll be appending
+        var c = str[i];
 
-    	// If it's a letter...
-    	if (c.match(/[a-z]/i)) {
+        // If it's a letter...
+        if (c.match(/[a-z]/i)) {
 
-    		// Get its code
-    		var code = str.charCodeAt(i);
+            // Get its code
+            var code = str.charCodeAt(i);
 
-    		// Uppercase letters
-    		if ((code >= 65) && (code <= 90))
-    			c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+            // Uppercase letters
+            if ((code >= 65) && (code <= 90))
+                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
 
-    		// Lowercase letters
-    		else if ((code >= 97) && (code <= 122))
-    			c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+            // Lowercase letters
+            else if ((code >= 97) && (code <= 122))
+                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
 
-    	}
+        }
 
-    	// Append
-    	output += c;
+        // Append
+        output += c;
     }
-    	document.getElementById("result").innerHTML = output;
-    	//return output;
-    	
+        document.getElementById("result").innerHTML = output;
+        //return output;
+        
     }
 }
 
@@ -105,14 +105,20 @@ function rowShift(n, row_index) {
         - Builds the Matrix based on a msg (string)
 */
 function buildMatrix(msg) {
-    var iM = 0; // row of M
-    var auxRow = [];
-    for (var i = 0; i  < msg.length; i++) {
-        if (i % 3 == 0) { // Each 4 characters adds the array to the M array to form the Matrix
-            M[iM].push(auxRow);
-            iM++;
-            auxRow = []; 
+    var msgIndex = 0;
+    for(var i = 0; i < 4; i++) {
+        M[i] = [];
+        for(var j=0; j < 4; j++) {
+            if (msgIndex < msg.length) {
+                M[i][j] = msg.charAt(msgIndex);
+            }
+            else {
+                M[i][j] = " ";
+            }
+            msgIndex++;
         }
-        auxRow.push(msg.charAt(i));
     }
 }
+
+buildMatrix("hola todos");
+console.log(M);
