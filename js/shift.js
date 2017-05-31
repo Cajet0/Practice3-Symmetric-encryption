4f58b5956221f37d8e6b4d6ea4a6f6297c7f4df5
Oliver Mendoza <oliver.r.2@hotmail.com>
2017-05-31 20:14:04 +0200

    horizontal shift fixed

--- js/shift.js
+++ js/shift.js
@@ -1,10 +1,15 @@
+/////////////// REMINDER ///////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ANY DIRECTLY COPY YOU MAKE OF AN OBJECT WILL BE A REFERENCE TO THE OBJECT, SO THINK BEFORE PROGRAM
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var str, opc;
 var M = []; // Message block
+var auxM = [] ; // Copy of matrix M to make the horizonal shift
 
 /*function magia(pal, opcion){
     str = pal;
-    if(opcion.localeCompare("Encriptar") == 0) caesarShift(pal,2);
-    if(opcion.localeCompare("Desencriptar") == 0) caesarShift(pal,-2);
+    if(opcion.localeCompare("Encriptar") == 0) caesarShift(pal, 2);
+    if(opcion.localeCompare("Desencriptar") == 0) caesarShift(pal, -2);
 }*/
 
 function caesarShift(str, amount) {
@@ -61,19 +66,21 @@ function horizontalShiftMatrix() {
 
 /* 
 getNumberRowShifts function:
-    - Calculate the number of shifts of a row "i" according to the value obtained evaluating a polynomial function in "i"
+    - Calculate the number of shifts of a row "i" according to the value obtained evaluating a specific polynomial function in "i" depending on if its odd or even
     - Then calls function rowShift to do the proper rowShift
 */
 function getNumberRowShifts() {
-    var suma;
     for (var i = 0; i < 4; i++) {
         var n;
         if (i % 2 == 0) { // Even
-            n = Math.pow(i^2) - 1;
+            n = Math.pow(i, 2) - 1;
+            //console.log("even");
         }
         else { // Odd
-            n = Math.poth(i^2) - 3*i + 2;
+            n = Math.pow(i, 2) - 3*i + 2;
+            //console.log("odd");
         }
+        //console.log("soy"+ n);
         rowShift(n, i);
     }
 }
@@ -85,17 +92,20 @@ rowShift function:
         row_index = row index of the matrix to be shifted
 */
 function rowShift(n, row_index) {
-    var auxM = M;
     if (n > 0) {
-        for (var j = 0; j < n; j++) {
+        for (var j = 0; j < 4; j++) {
             var new_colum_index = (j + n) % 4;
-            M[row_index, new_colum_index] = auxM[row_index, j]; 
+            //console.log("n>0 " + new_colum_index);
+            //console.log(M);
+            //console.log(row_index);
+            M[row_index][new_colum_index] = auxM[row_index][j]; 
         }
     }
     else if (n < 0) {
-        for (var j = 0; j < n; j++) {
-            var new_colum_index = (j + (4-n)) % 4;
-            M[row_index, new_colum_index] = auxM[row_index, j]; 
+        for (var j = 0; j < 4; j++) {
+            var new_colum_index = (j + (4+n)) % 4;
+            //console.log("n<0"+ " "+ new_colum_index);
+            M[row_index][new_colum_index] = auxM[row_index][j]; 
         }
     }
 }
@@ -118,26 +128,36 @@ function buildMatrix(msg) {
             msgIndex++;
         }
     }
-    transpuesta(M);
+    for (var i = 0; i<4; i++) {
+        auxM[i] = [];
+        for (var j = 0; j < 4; j++) {
+            auxM[i][j] = M[i][j];
+        }
+    }
+    console.log(auxM);
 }
 
 /* transpuesta function:
-		- Builds the transposed matrix based on the matrix M
+    - Builds the transposed matrix based on the matrix M
 */
 function transpuesta(matrix) {
     var transp = new Array(4);
-	// create the matrix
+    // create the matrix
     for(var i=0; i<4; i++) {
         transp[i] = new Array(4);
     }
-	// fill the matrix
+    // fill the matrix
     for(var i=0; i<4; i++){
         for(var j=0; j<4; j++){
             transp[i][j] = matrix[j][i];
         }
     }
-    alert(transp);
+    M = transp;
 }
 
-buildMatrix("hola todos");
+//buildMatrix("hola a todos"); // Creates matrix
+buildMatrix("jesus oliver dan");
+horizontalShiftMatrix();
+//transpuesta(M);
 console.log(M);
+console.log(auxM);
