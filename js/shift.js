04e7a7d08420b746a93dd9dee4d235a31ffae2cc
Oliver Mendoza <oliver.r.2@hotmail.com>
2017-06-04 00:28:21 +0200

    union de todo

--- js/shift.js
+++ js/shift.js
@@ -1,60 +1,103 @@
 /////////////// REMINDER ///////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ANY DIRECTLY COPY YOU MAKE OF AN OBJECT WILL BE A REFERENCE TO THE OBJECT, SO THINK BEFORE PROGRAM
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var str, opc;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+var str, opc, pal;
+var output = "";
+var c="";
 var M = []; // Message block
 var auxM = [] ; // Copy of matrix M to make the horizonal shift
 
-/*function magia(pal, opcion){
-    str = pal;
-    if(opcion.localeCompare("Encriptar") == 0) caesarShift(pal, 2);
-    if(opcion.localeCompare("Desencriptar") == 0) caesarShift(pal, -2);
-}*/
-
-function caesarShift(str, amount) {
-
-    if((str.length < 5) || (str.length > 20 )){
-            alert('Palabra entre cinco y veinte caracteres');
+function borra(){
+    document.getElementById("palabra").value = "";
+    $("#result").text("Result Here");
+    var M = [];
+    for(var i=0; i<4; i++) {
+        M[i] = new Array(4);
     }
-    else {
-        // Wrap the amount
-    if (amount < 0)
-        return caesarShift(str, amount + 26);
+    output = "";
+}
 
-    // Make an output variable
-    var output = '';
+function magia(){
+    var M = [];
+    for(var i=0; i<4; i++) {
+        M[i] = new Array(4);
+    }
+    output = "";
+    str = document.getElementById('palabra').value;
+    opc = document.getElementById('opcion').value;
 
-    // Go through each character
-    for (var i = 0; i < str.length; i ++) {
-
-        // Get the character we'll be appending
-        var c = str[i];
-
-        // If it's a letter...
-        if (c.match(/[a-z]/i)) {
-
-            // Get its code
-            var code = str.charCodeAt(i);
-
-            // Uppercase letters
-            if ((code >= 65) && (code <= 90))
-                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-
-            // Lowercase letters
-            else if ((code >= 97) && (code <= 122))
-                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+    if(opc == "Encrypt") 
+        {
+            //alert("Encriptar");
+            //caesarShift(str,2);
+            //CICLO FOR PARA RECORRER LA PALABRA
+            for(var i =0; i < str.length ; i++){
+                if(i%2 != 0){
+                    //alert(str.charAt(i));
+                    //alert("Position : "+parseInt(i/4)+","+parseInt(i%4))
+                    var sum = parseInt(i/4) + parseInt(i%4); 					//SUMA RENGLON´+ COLUMNA
+                    //alert("Par Suma: "+sum);
+                    var tamaño = Math.pow(sum,2) - 2*sum; 						//FUNCION POLINOMIAL DEFINIDA PREVIAMENTE
+                    //alert("Par TamShift: " +tamaño);
+                    var x = caesarShift(str.charAt(i),parseInt(tamaño%15)); 	//MANDAS LLAMAR EL CAESAR SHIFT
+                    //alert("Par char ya encriptado:"+x);
+                    M[parseInt(i/4)][parseInt(i%4)] = x 						//Asignas el character ya coificado a la matriz
+                }
+                else{
+                    //alert(str.charAt(i));
+                    //alert("Position : "+parseInt(i/4)+","+parseInt(i%4))
+                    var sum = parseInt(i/4) + parseInt(i%4);					//SUMA RENGLON´+ COLUMNA
+                    //alert("Impar Suma: " +sum);
+                    var tamaño = Math.pow(sum,2) - 3*sum + 2;					//FUNCION POLINOMIAL DEFINIDA PREVIAMENTE
+                    //alert("Impar TamShift: "+tamaño);
+                    var y = caesarShift(str.charAt(i),parseInt(tamaño%15));		//MANDAS LLAMAR EL CAESAR SHIFT
+                    //alert("Impar char ya encriptado: "+y);
+                    M[parseInt(i/4)][parseInt(i%4)] = y;						//Asignas el character ya coificado a la matriz
+                }
+            }
+            alert("Array: "+M);
+    }
+    if(opc == "Decrypt") 
+        {
 
         }
 
-        // Append
-        output += c;
-    }
-        document.getElementById("result").innerHTML = output;
-        //return output;
-        
-    }
+    $("#result").text(output);
 }
+
+/**
+The function receives  a letter and gets the Ascii number asociated to that character, then made the change (shift)
+based on the amount parameter.
+**/
+
+function caesarShift(str, amount) {
+    // Get its code
+    c="";
+    var code = str.charCodeAt(0);
+    //alert("Char en numero "+ code);
+    //alert("Amount Caesar Shift: "+amount);
+            if ((code >= 32) && (code <= 64))
+            c = String.fromCharCode(((code - 32 + amount) % 33) + 32);	
+
+            // Uppercase letters
+             if ((code >= 65) && (code <= 90))
+                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+            // More Characters
+             if ((code >= 91) && (code <= 96))
+                c = String.fromCharCode(((code - 91 + amount) % 6) + 91);
+
+            // Lowercase letters
+             if ((code >= 97) && (code <= 122))
+                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+    //alert("Codigo ya encriptado: "+c)
+    output += c;
+
+    return c;
+}
+
 
 /*
 horizontalShiftMatrix function:
@@ -128,7 +171,7 @@ function buildMatrix(msg) {
             msgIndex++;
         }
     }
-    for (var i = 0; i<4; i++) {
+    for (var i = 0; i<4; i++) {
         auxM[i] = [];
         for (var j = 0; j < 4; j++) {
             auxM[i][j] = M[i][j];
@@ -155,9 +198,11 @@ function transpuesta(matrix) {
     M = transp;
 }
 
+/*
 //buildMatrix("hola a todos"); // Creates matrix
 buildMatrix("jesus oliver dan");
 horizontalShiftMatrix();
 //transpuesta(M);
 console.log(M);
 console.log(auxM);
+*/
